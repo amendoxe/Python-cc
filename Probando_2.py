f35da73ff487f56c65e2c5e4944f995354632505
amendoxe <arturo_mendoza@outlook.com>
2023-04-05 22:48:15 -0600

    and some dictionary looping

--- Probando_2.py
+++ Probando_2.py
@@ -1,4 +1,5 @@
 # Probando 2
+import random
 my_list = ['apple', 'banana', 'cherry']
 """ Por si las dudas, es importante crear una variable que guarde un valor
 si se va a llamar varias veces para evitar que la función asociada sea
@@ -6,3 +7,16 @@ ejecutada varias veces """
 list_size = len(my_list)
 for i in range(list_size):
     print(f"The item {i} is {my_list[i]}")
+
+# Ahora vamos a probar un poco de diccionarios
+
+my_dict = {
+    "Alice": "Python",
+    "Bob": "Java",
+    "Charlie": "JavaScript",
+    "Dave": "C++",
+    "Eve": "Ruby"
+}
+
+for name in my_dict:
+    print(name)
