810687c0d93dcf58bb45b51974dc38e088b06ac6
amendoxe <arturo_mendoza@outlook.com>
2023-04-25 17:01:56 -0600

    MOAR

--- parrot.py
+++ parrot.py
@@ -1,3 +1,7 @@
 # parrot on cmd
-message = input("Escribe un mensaje y te lo repetiré: ")
-print(message)
+prompt = "Write something, and I will write it back"
+prompt += f"\nType 'quit' to exit program: "
+message = ""
+while message != "quit":
+    message = input(prompt)
+    print(message)
