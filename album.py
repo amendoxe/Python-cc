fae6efd045a2a33acdf9a5d689b4d660d44c53d0
amendoxe <arturo_mendoza@outlook.com>
2023-05-07 21:13:42 -0600

    add the album excercise

---
+++ album.py
@@ -0,0 +1,24 @@
+# 8-7 function album(name, title, #s), builds a dictionary of music album
+# return album dictionary
+albums = []
+
+
+def make_album(band_name, disc_title, tracks=None):
+    list_size = len(albums)
+    album = {"name": band_name, "album": disc_title}
+    if tracks:
+        album["number of tracks"] = tracks
+    albums.append(f"album{list_size}")
+    print(album)
+    return list_size
+
+
+while True:
+    name = input("Escribe el nombre de la banda: ")
+    title = input("Escribe el nombre del disco: ")
+    tracks = input("Numero de canciones, pulsa enter para omitir: ")
+    continuing = input("q to quit: ")
+    make_album(name, title, tracks)
+    print(albums)
+    if continuing == "q":
+        break
