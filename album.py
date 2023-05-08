f9db56d4141b315733d99d4228653f8adb8355b4
amendoxe <arturo_mendoza@outlook.com>
2023-05-08 00:10:59 -0600

    some linting to album

--- album.py
+++ album.py
@@ -1,16 +1,17 @@
-# 8-7 function album(name, title, #s), builds a dictionary of music album
-# return album dictionary
+
+"""8-7 function album(name, title, #s), builds a dictionary of music album
+# return album dictionary """
 albums = []
 
 
 def make_album(band_name, disc_title, tracks=None):
-    list_size = len(albums)
+    """ create a dictionary with the user input """
+
     album = {"name": band_name, "album": disc_title}
     if tracks:
         album["number of tracks"] = tracks
     albums.append(album)
     print(f"This is the last album you recorded \n {album}\n")
-    return list_size
 
 
 while True:
@@ -19,8 +20,8 @@ while True:
     track = input("Numero de canciones, pulsa enter para omitir: ")
     make_album(name, title, track)
     print("This are all the albums you have recorded so far:\n")
-    for album in albums:
-        print(album)
+    for al in albums:
+        print(al)
     continuing = input("Continue?: y/n ")
     if continuing == "n":
         break
