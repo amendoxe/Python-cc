6e61b2ee17282a4890056ad621e64520e060fad1
amendoxe <arturo_mendoza@outlook.com>
2023-05-10 22:51:57 -0600

    Add imports.py for practice

---
+++ imports.py
@@ -0,0 +1,10 @@
+""" 8-16 practice imports with my functions.py """
+import my_functions
+test_string = "Hi there, this is a string and it is happy to see you ☺️"
+test_list = ["uno", "dos", "tres", "cuatro", "cinco"]
+test_dictionary = {"name": "timmy", "last name": "turner",
+                   "age": 10, "profession": "kid"}
+
+my_functions.print_string(test_string)
+my_functions.print_list(test_list)
+my_functions.print_dict(test_dictionary)
--- my_functions.py
+++ my_functions.py
@@ -3,16 +3,19 @@
 
 def print_string(the_string):
     """ simple print """
+    print("\n printing the string")
     print(the_string.title())
 
 
 def print_list(the_list):
     """ print all the elements of the_list """
+    print("\nprinting the list")
     for i in the_list:
         print(i)
 
 
 def print_dict(the_dict):
     """ print all the elements of the_dict """
+    print("\n printing the dictionary:")
     for i, j in the_dict.items():
         print(f"The key is {i}, the value is {j}")
