0f8b20c792294597a11ebf1f34517b64db34fd19
amendoxe <arturo_mendoza@outlook.com>
2023-05-15 22:20:42 -0600

    Add imports 2 & 3

---
+++ imports2.py
@@ -0,0 +1,12 @@
+""" Importing practice """
+from my_functions import print_string
+from my_functions import print_list
+from my_functions import print_dict
+test_string = "Hi there, this is a string and it is happy to see you ☺️"
+test_list = ["uno", "dos", "tres", "cuatro", "cinco"]
+test_dictionary = {"name": "timmy", "last name": "turner",
+                   "age": 10, "profession": "kid"}
+
+print_string(test_string)
+print_list(test_list)
+print_dict(test_dictionary)
---
+++ imports3.py
@@ -0,0 +1,13 @@
+""" Importing more practice """
+from my_functions import print_string as ps
+from my_functions import print_list as pl
+from my_functions import print_dict as pd
+
+test_string = "Hi there, this is a string and it is happy to see you ☺️"
+test_list = ["uno", "dos", "tres", "cuatro", "cinco"]
+test_dictionary = {"name": "timmy", "last name": "turner",
+                   "age": 10, "profession": "kid"}
+
+ps(test_string)
+pl(test_list)
+pd(test_dictionary)
