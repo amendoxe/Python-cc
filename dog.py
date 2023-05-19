59af7c7000792e53efe09dd3d5c2323c67893c7b
amendoxe <arturo_mendoza@outlook.com>
2023-05-19 13:01:23 -0600

    Some changes to dog.py

--- dog.py
+++ dog.py
@@ -10,8 +10,19 @@ class Dog:
 
     def sit(self):
         """makes dog sit"""
-        print(f"{self.name} sits")
+        print(f"{self.name} is now sitting.")
 
     def roll(self):
         """makes dog roll"""
         print(f"{self.name} rolls!")
+
+
+my_dog = Dog("Jake", 5)
+my_dog.roll()
+my_dog.sit()
+print("my dogs name is:", my_dog.name)
+print("my dogs age is:", my_dog.age)
+
+your_dog = Dog("Lucy", 3)
+your_dog.sit()
+your_dog.roll()
