ff42f30fb6083a822496dd73cf9aa86d2e86f5ab
amendoxe <arturo_mendoza@outlook.com>
2023-06-02 08:56:33 -0600

    Add exception handling to guess_number :P and something to testing_against_script.ipynb

--- guess_number.py
+++ guess_number.py
@@ -4,17 +4,21 @@ import random
 import sys
 
 number_iam_thinking = random.randint(1, 100)
+
 while True:
-    print("which number am I thinking of: "
+    print("\nwhich number am I thinking of(1-100)? "
           "\n type 'q' to quit")
-    user_input = input()
+    user_input = input(": ")
     if user_input == "q":
         break
-    elif int(user_input) > number_iam_thinking:
-        print("too high")
-    elif int(user_input) < number_iam_thinking:
-        print("too low")
-    else:
-        print("Congratulations, you guessed it,"
-              f"the number is {number_iam_thinking}")
-        sys.exit()
+    try:
+        if int(user_input) > number_iam_thinking:
+            print("too high")
+        elif int(user_input) < number_iam_thinking:
+            print("too low")
+        elif int(user_input) == number_iam_thinking:
+            print("Congratulations, you guessed it!,"
+                  f"the number is {number_iam_thinking}")
+            sys.exit()
+    except ValueError:
+        print("\n\tThat certainly is not a number, try again!")
